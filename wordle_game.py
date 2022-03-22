2178020a9bd0ee10d3d46203d19e049a8490be9b
magn3144 <magnus@elgaard.com>
2022-03-22 16:33:41 +0100

    Koden virker, men har ikke kørt den fordi min computer er for langsom

--- wordle_game.py
+++ wordle_game.py
@@ -2,19 +2,35 @@ import numpy as np
 
 class wordle:
     def __init__(self):
-        self.word = np.random.choice(self.load_words())
+        with open('words.txt') as word_file:
+            self.all_possible_words = list(word_file.read().split())
+        self.word = np.random.choice(self.all_possible_words)
+        self.guesses = [None] * 6
+        self.all_info = np.zeros((6, 5), dtype='int')
+        self.n_guesses = 0
 
-    def load_words(self):
-        with open('wordlist.txt') as word_file:
-            return list(word_file.read().split())
-    
-    def make_guess(self, guess):
-        info = []
-        for letter_self, letter_guess in zip(self.word, guess):
-            if letter_self == letter_guess:
-                info.append(2)
-            elif letter_guess in self.word:
-                info.append(1)
-            else:
-                info.append(0)
+    def check_answer(self, correct_word, guess):
+        info = np.zeros(5, dtype='int')
+        for i in range(5):
+            if correct_word[i] == guess[i]:
+                info[i] = 2
+            elif guess[i] in correct_word:
+                info[i] = 1
         return info
+
+    def make_guess(self, guess):
+        self.all_info[self.n_guesses] = self.check_answer(self.word, guess)
+        self.guesses[self.n_guesses] = guess
+        self.n_guesses += 1
+    
+    def is_possible(self, word, simulating):
+        for i in range(self.n_guesses + simulating):
+            for j in range(self.all_info.shape[1]):
+                if self.all_info[i, j] == 2 and self.guesses[i][j] != word[j]:
+                    return False
+                if self.all_info[i, j] == 1 and (not self.guesses[i][j] in word or self.guesses[i][j] == word[j]):
+                    return False
+                if self.all_info[i, j] == 0 and self.guesses[i][j] in word:
+                    return False
+
+        return True
--- wordle_solver.py
+++ wordle_solver.py
@@ -1,4 +1,26 @@
 from wordle_game import wordle
+import numpy as np
+
+def solve_wordle():
+    env = wordle()
+    all_info = np.full((6, 5), -1)
+    for i in range(6):
+        information_gain_sums = np.zeros(len(env.all_possible_words), dtype='int')
+        for j, possible_guess in enumerate(env.all_possible_words):
+            for possible_word in env.all_possible_words:
+                env.guesses[env.n_guesses] = possible_guess
+                env.all_info[env.n_guesses] = env.check_answer(possible_word, possible_guess)
+                n_impossible_words = 0
+                for word in env.all_possible_words:
+                    if not env.is_possible(word, True):
+                        n_impossible_words += 1
+                information_gain_sums[j] += n_impossible_words
+        env.make_guess(env.all_possible_words[np.argmax(information_gain_sums)])
+        for j in range(len(env.all_possible_words) - 1, -1, -1):
+            if not env.is_possible(env.all_possible_words[j], False):
+                del env.all_possible_words[j]
+        print(env.guesses[i])
+        print(len(env.all_possible_words))
 
 if __name__ == '__main__':
-    env = wordle()
+    solve_wordle()
